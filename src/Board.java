c55319876ed22d41c53b449ec8c4f4368350ebef
The Machine <xandxtreme@gmail.com>
2018-06-26 22:06:10 -0400

    git commit -m "first commit"

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,4 @@
 public class Board
 {
-    private int chessBoard[][];
-    /*
 
-     */
-    public static void getBoard()
-    {
-
-    }
 }
---
+++ src/Field.java
@@ -0,0 +1,17 @@
+public class Field
+{
+    public String color;
+    public boolean occupancy;
+    public String address;
+
+    /*
+    Constructor method that takes three parameters: color, occupancy and address.
+     */
+    public static void field(String colour, boolean occupied, String adres)
+    {
+        color = colour;
+        occupancy = occupied;
+        address = adres;
+    }
+
+}
